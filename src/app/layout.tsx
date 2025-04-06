2e5c06ecbaff00947ec04f20abb591d4c6f37244
Fimil Faneea <fimilfaneea@gmail.com>
2025-04-06 14:20:00 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
